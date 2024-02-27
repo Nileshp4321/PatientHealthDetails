1b51c6c5269ee978542a1c55cb606cdc08eba396
Nileshlejhro <nilesh.pushkarna@lejhro.com>
2024-02-27 13:36:48 +0530

    Add icon for patient and appointment date and time and use axios to fetch data

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import AppointmentsChart from './AppointmentsChart/AppointmentsChart';
+
+function App() {
+  return (
+   <AppointmentsChart/>
+  );
+}
+
+export default App;
